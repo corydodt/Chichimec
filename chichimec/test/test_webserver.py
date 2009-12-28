e7a3fd8c4e12ed16dc7a6e2b66464fbf876f792d
Cory Dodt <corymercurial@spam.goonmill.org>
2009-12-27 21:17:01 -0800

    100% coverage of webserver module

--- chichimec/test/test_webserver.py
+++ chichimec/test/test_webserver.py
@@ -14,41 +14,61 @@ class WebsiteTest(unittest.TestCase):
     """
     The website starts up and does correctly limit logs
     """
-    # def setUp(self):
+    def setUp(self):
+        self.messages = []
+        def msg(*a, **kw):
+            self.messages.append([a, sorted(kw.items()),
+                ])
+
+        Root = fudge.Fake('Root', callable=True).returns(fudge.Fake('Resource'))
+        self.restore1 = fudge.patch_object(resource, 'Root', Root)
+        self.restore2 = fudge.patch_object(log, 'msg', msg)
+
+    def tearDown(self):
+        self.restore2.restore()
+        self.restore1.restore()
+        self.Root = None
+        del self.messages[:]
 
     def test_logging(self):
         """
         Our website has 80-column logging
         """
-        Root = fudge.Fake('Root', callable=True).returns(fudge.Fake('Resource'))
         req = fudge.Fake('request')
         req.uri = 'http://foob'
         req.code = 200
-        messages = []
 
-        def msg(*a, **kw):
-            messages.append([a, sorted(kw.items()),
-                ])
+        from chichimec.webserver import WebSite
+        ws = WebSite()
+        ws.log(req)
+        self.assertEqual(self.messages, [[('200 http://foob',),
+            [('system', 'HTTP')]]])
 
-        with fudge.patched_context(resource, 'Root', Root):
-            with fudge.patched_context(log, 'msg', msg):
-                from chichimec.webserver import WebSite
-                ws = WebSite()
-                ws.log(req)
-                self.assertEqual(messages, [[('200 http://foob',),
-                    [('system', 'HTTP')]]])
+        del self.messages[:]
 
-                del messages[:]
+        req.code = 500
+        ws.log(req)
+        self.assertEqual(self.messages, [[('!500! http://foob',),
+            [('system', 'HTTP')]]])
 
-                req.code = 500
-                ws.log(req)
-                self.assertEqual(messages, [[('!500! http://foob',),
-                    [('system', 'HTTP')]]])
+        del self.messages[:]
 
-                del messages[:]
+        req.uri = 'http://exeptionally/looooooooooooooooooooooooooooooooooooooooong'
+        ws.log(req)
+        self.assertEqual(self.messages, [[('!500! ...ooooooooooooooooooooooooooooooooong',),
+            [('system', 'HTTP')]]])
 
-                req.uri = 'http://exeptionally/looooooooooooooooooooooooooooooooooooooooong'
-                ws.log(req)
-                self.assertEqual(messages, [[('!500! ...ooooooooooooooooooooooooooooooooong',),
-                    [('system', 'HTTP')]]])
+    def test_jsmodule(self):
+        """
+        Special handling for 'jsmodule' in the url should be correct
+        """
+        req = fudge.Fake('request')
+        req.uri = 'http://foob/jsmodule/0127109831029/TestProject'
+        req.code = 200
+
+        from chichimec.webserver import WebSite
+        ws = WebSite()
+        ws.log(req)
+        self.assertEqual(self.messages, [[('200 .../jsmodule/0127109831029/TestProject',),
+            [('system', 'HTTP')]]])
 
