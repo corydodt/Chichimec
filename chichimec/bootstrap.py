f6f4e4feb36087b87aced274d12b39f5343138ed
Cory Dodt <corymercurial@spam.goonmill.org>
2010-01-01 00:07:21 -0800

    clean up bootstrap and fix a bug with flags

--- chichimec/bootstrap.py
+++ chichimec/bootstrap.py
@@ -36,7 +36,7 @@ def parseFlags(f):
     for val in vals:
         if val == '~':
             continue
-        elif '=' in f:
+        elif '=' in val:
             k, v = val.split('=', 1)
             r[k] = v
         else:
@@ -55,29 +55,34 @@ def generate(options):
             source, target, flags = line.split()
             if flags:
                 flags = parseFlags(flags)
+            source = 'YourProject/' + source
             data = open(RESOURCE(source)).read()
             if not flags.get('no-format'):
                 data = data.format(options=options)
+            target = options['projectDir'] + '/' + target
             filename = target.format(options=options)
             yield flags, filename, data
 
 
 generatedFiles = ( # {{{
 """
-YourProject/setup.py    {options[projectDir]}/setup.py  ~
-YourProject/yourproject/__init__.py {options[projectDir]}/{options[projectName]}/__init__.py    ~
-YourProject/yourproject/resource.py {options[projectDir]}/{options[projectName]}/resource.py    ~
-YourProject/twisted/plugins/yourprojecttp.py {options[projectDir]}/twisted/plugins/{options[projectName]}tp.py  ~
-YourProject/nevow/plugins/yourprojectnp.py {options[projectDir]}/nevow/plugins/{options[projectName]}np.py  ~
-YourProject/yourproject/test/__init__.py {options[projectDir]}/{options[projectName]}/test/__init__.py  best-practices
-YourProject/yourproject/test/test_yourproject.py {options[projectDir]}/{options[projectName]}/test/test_{options[projectName]}.py  best-practices
-YourProject/yourproject/test/test_twistdplugin.py {options[projectDir]}/{options[projectName]}/test/test_twistdplugin.py   best-practices
-YourProject/yourproject/test/test_nevowplugin.py {options[projectDir]}/{options[projectName]}/test/test_nevowplugin.py best-practices
-YourProject/yourproject/static/jquery-1.3.2.js {options[projectDir]}/{options[projectName]}/static/jquery-1.3.2.js jquery,no-format
-YourProject/yourproject/static/YourProject/__init__.js {options[projectDir]}/{options[projectName]}/static/{options[projectDir]}/__init__.js jquery,no-format
-YourProject/yourproject/static/css/yourproject.css  {options[projectDir]}/{options[projectName]}/static/css/{options[projectName]}.css  ~
-YourProject/yourproject/templates/yourproject.xhtml {options[projectDir]}/{options[projectName]}/templates/{options[projectName]}.xhtml ~
-YourProject/README      {options[projectDir]}/README    ~
-YourProject/runtests    {options[projectDir]}/runtests  mode=0o755
-YourProject/.hgignore   {options[projectDir]}/.hgignore best-practices
+setup.py                                    setup.py                                                         ~
+README                                      README                                                           ~
+
+yourproject/__init__.py                     {options[projectName]}/__init__.py                               ~
+yourproject/resource.py                     {options[projectName]}/resource.py                               ~
+yourproject/static/css/yourproject.css      {options[projectName]}/static/css/{options[projectName]}.css     ~
+yourproject/templates/yourproject.xhtml     {options[projectName]}/templates/{options[projectName]}.xhtml    ~
+nevow/plugins/yourprojectnp.py              nevow/plugins/{options[projectName]}np.py                        ~
+twisted/plugins/yourprojecttp.py            twisted/plugins/{options[projectName]}tp.py                      ~
+
+yourproject/static/jquery-1.3.2.js          {options[projectName]}/static/jquery-1.3.2.js                    jquery,no-format
+yourproject/static/YourProject/__init__.js  {options[projectName]}/static/{options[projectDir]}/__init__.js  jquery,no-format
+
+runtests                                    runtests                                                         mode=0o755,best-practices
+.hgignore                                   .hgignore                                                        best-practices
+
+yourproject/test/__init__.py                {options[projectName]}/test/__init__.py                          best-practices
+yourproject/test/test_twistdplugin.py       {options[projectName]}/test/test_twistdplugin.py                 best-practices
+yourproject/test/test_nevowplugin.py        {options[projectName]}/test/test_nevowplugin.py                  best-practices
 """) # }}}
