0033ac6f405f912c6e9de33cc9061f71af401005
Cory Dodt <corymercurial@spam.goonmill.org>
2010-01-01 03:04:25 -0800

    #7be txgenshi page test

---
+++ chichimec/example.py
@@ -0,0 +1,94 @@
+"""
+Example page resources.  Boilerplate apps will import these.
+"""
+from datetime import datetime
+import random
+
+from twisted.internet import task
+
+from nevow import athena, rend, loaders
+
+from chichimec.util import RESOURCE
+
+from txgenshi.loader import genshifile
+
+class GenshiMixPage(rend.Page):
+    """
+    Example page with genshi templates demo
+    """
+    docFactory = genshifile(RESOURCE('templates/genshimix.xhtml'))
+    addSlash = True
+
+    def __init__(self, *args, **kwds):
+        rend.Page.__init__(self, *args, **kwds)
+        self.key = {'value': 'this is the value of the key'}
+        self.doIt = True
+        self.items = xrange(10)
+        evenOdd = ['odd', 'even']
+        self.altItems = [ (x, {'class': evenOdd[x % 2]})
+            for x in list(xrange(10))]
+        self.username = u'Joe'
+
+    def date(self):
+        """
+        Right now, formatted as a dotted date
+        """
+        return datetime.now().strftime("%Y.%m.%d %H:%M:%S")
+
+    date = property(date)
+
+    def render_username(self, context, data):
+        """
+        It's Joe.
+        """
+        return self.username
+
+
+class AthenaPage(rend.Page):
+    """
+    Example page with athena demo
+    """
+    docFactory = genshifile(RESOURCE('templates/athena.xhtml'))
+
+    def render_random(self, ctx, data):
+        """
+        The demo widget
+        """
+        wid = MyWidget()
+        wid.setFragmentParent(self)
+        return ctx.tag[wid]
+
+class RandomNumber(athena.LiveElement):
+    """
+    Generate random numbers on the server.  When one of them matches a
+    particular pattern, send it to the client.
+    """
+    running = None
+    jsClass = 'Chichimec.RandomNumber'
+    docFactory = loaders.xmlfile(RESOURCE('templates/RandomNumber'))
+
+    @athena.expose
+    def setFilter(self, num):
+        """
+        Set a new number filter.  num should be an integer 3 digits or less,
+        it will be compared with the final digits of the numbers this widget
+        generates on the server.
+
+        If this has never been called before, it also starts the ticker
+        running.
+        """
+        self.filter = num
+        if not self.running:
+            self.running = self.task.start(0.2)
+        return 'ok'
+
+    def number(self):
+        """
+        Generate a number
+        """
+        r = int(random.random() * 1000000)
+        if str(r).endswith(str(self.filter)):
+            d = self.callRemote('number', r)
+
+    def __init__(self, *a, **kw):
+        self.task = task.LoopingCall(self.number)
--- chichimec/test/test_example.py
+++ chichimec/test/test_example.py
@@ -5,7 +5,7 @@ from twisted.trial import unittest
 
 import re
 
-from chichimec import resource
+from chichimec import example
 
 class ResourceTest(unittest.TestCase):
     """Tests of the generated examples"""
@@ -15,27 +15,29 @@ class ResourceTest(unittest.TestCase):
         """
         Templates generated with txGenshi work
         """
-        r = resource.ExampleResource()
+        r = example.GenshiMixPage()
         s = r.renderSynchronously()
         self.assertSubstring('<p>You are Joe and your stuff is here.</p>', s)
         self.assertSubstring('<span>Your name is Joe</span>', s)
         self.assertSubstring('<span>this is the value of the key</span>', s)
-        self.assertSubstring('<span>7 49 59</span>', s)
+        self.assertSubstring('<span>49 7 59</span>', s)
         self.assertSubstring('Yeah, we can do it!', s)
-        self.failUnlessSubstring("Aw shucks, we can't do it.", s)
-        self.assertSubstring('<li>0</li>', s); self.assertSubstring('<li>9</li>', s)
-        self.assertTrue(re.search(r'<p>\s*Hello, world1\s*</p>', s))
-        self.assertTrue(re.search(r'<p>\s*Hello, everyone else!\s*</p>', s))
-        self.assertTrue(re.search(r'<p>\s*Hello, Dude!\s*</p>', s))
-        self.assertTrue(re.search(r'<p>\s*Hello again, Dude!\s*</p>', s))
-        self.assertSubstring('<li class="even">item 0</li>', s)
-        self.assertSubstring('<li class="odd">item 9</li>', s)
+        self.failIfSubstring("Aw shucks, we can't do it.", s)
+        self.assertSubstring('<li>item 0</li>', s)
+        self.assertSubstring('<li>item 9</li>', s)
+        self.assertTrue(re.search(r'<p class="greeting">\s*Hello, world!\s*</p>', s))
+        self.assertTrue(re.search(r'<p class="greeting">\s*Hello, everyone else!\s*</p>', s))
+        self.assertTrue(re.search(r'<span>\s*Hello Dude\s*</span>', s))
+        self.assertTrue(re.search(r'<span>\s*Hello again, Dude\s*</span>', s))
+        self.assertSubstring('<li class="odd">item 0</li>', s)
+        self.assertSubstring('<li class="even">item 9</li>', s)
         self.assertSubstring('<li>Joe</li>', s)
-        self.assertTrue(re.search(r'<h2>Genshi :replace</h2>\s*Joe\s*</ul>'))
-        self.assertTrue(re.search(r'<h2>Genshi :strip</h2>\s*<b>foo</b>\s*</div>'))
+        self.assertTrue(re.search(r'<h2>Genshi :replace</h2>\s*Joe\s*</ul>', s))
+        self.assertTrue(re.search(r'<h2>Genshi :strip</h2>\s*<b>foo</b>\s*</div>', s))
 
     def test_athenaExample(self):
         """
         Comet-based app works
         """
         self.fail("You should write a test")
+        r = example.AthenaPage()
