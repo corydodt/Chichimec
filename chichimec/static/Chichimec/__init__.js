00f0035bd39b583855e3b19de5a26d0f1d9c5947
Cory Dodt <corymercurial@spam.goonmill.org>
2010-01-14 21:40:29 -0800

    whitespace

--- chichimec/static/Chichimec/__init__.js
+++ chichimec/static/Chichimec/__init__.js
@@ -1,27 +1,27 @@
  
 Nevow.Athena.Widget.subclass(Chichimec, 'RandomNumber').methods(
-        function __init__(self, node) {
-            Chichimec.RandomNumber.upcall(self, '__init__', node);
-            self.filterChanged(12);
+    function __init__(self, node) {
+        Chichimec.RandomNumber.upcall(self, '__init__', node);
+        self.filterChanged(12);
 
-            var form = self.node.getElementsByTagName('form')[0];
-            form.onsubmit = function () {
-                var input = self.node.getElementsByTagName('input')[0];
-                var val = input.value;
-                self.filterChanged(val);
-                return false;
-            };
-        },
+        var form = self.node.getElementsByTagName('form')[0];
+        form.onsubmit = function () {
+            var input = self.node.getElementsByTagName('input')[0];
+            var val = input.value;
+            self.filterChanged(val);
+            return false;
+        };
+    },
 
-        function number(self, number) {
-            var n = self.node.getElementsByTagName('strong')[0];
-            n.innerHTML = number;
-            return 'ok';
-        },
+    function number(self, number) {
+        var n = self.node.getElementsByTagName('strong')[0];
+        n.innerHTML = number;
+        return 'ok';
+    },
 
-        function filterChanged(self, filter) {
-            self.callRemote('setFilter', filter).addCallback(
-                function (x) { self.number(x)
-            });
-        }
+    function filterChanged(self, filter) {
+        self.callRemote('setFilter', filter).addCallback(
+            function (x) { self.number(x)
+        });
+    }
 );
